a1a22ab80716093ae9e6d48e288d5b0ae61e1570
zl <zl@163.com>
2018-04-27 19:51:36 +0800

    testGit 新的提交

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,7 +2,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
+
         System.out.println("testGit2 第一次提交");
-        //System.out.println("testGit 新的提交");
+        System.out.println("testGit 新的提交");
     }
 }
