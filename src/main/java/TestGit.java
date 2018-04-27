20c5a06195c9d2e49ea032c9624eac1633165fd9
zl <zl@163.com>
2018-04-27 17:46:15 +0800

    TestGit2 第一次提交

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
+        System.out.println("testGit2 第一次提交");
     }
 }
